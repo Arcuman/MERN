7ae2d9e7dd9a9cd69cdcaa091cc2a3017a4c287e
Arcuman <46977740+Arcuman@users.noreply.github.com>
2021-01-31 11:15:16 +0300

    feat: add skeleton and start simplesetup

---
+++ mern-skeleton/server/server.js
@@ -0,0 +1,17 @@
+import config from './../config/config'
+import mongoose from 'mongoose'
+
+// Connection URL
+mongoose.Promise = global.Promise
+mongoose.connect(config.mongoUri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
+mongoose.connection.on('error', () => {
+    throw new Error(`unable to connect to database: ${config.mongoUri}`)
+})
+
+import app from './express'
+app.listen(config.port, (err) => {
+    if (err) {
+        console.log(err)
+    }
+    console.info('Server started on port %s.', config.port)
+})
